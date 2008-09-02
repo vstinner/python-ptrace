a587edd55d6f87a747863eb69ab0f343a80131ff
haypo <devnull@localhost>
2008-09-02 01:40:01 +0000

    socket test: add call to setsockopt()

--- tests/socket_ipv4_tcp.c
+++ tests/socket_ipv4_tcp.c
@@ -6,12 +6,16 @@
 int main()
 {
     int fd;
+    int val;
     int ret;
     struct sockaddr_in addr;
 
     fd = socket(PF_INET, SOCK_STREAM, 0);
     if (fd < 0)
         perror("socket");
+
+    val = 1;
+    (void)setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
     addr.sin_family = AF_INET;
     addr.sin_port = htons(80);
