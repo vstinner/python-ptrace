da06b1ea305c077114f4ae83e34b6df53bf58f24
haypo <devnull@localhost>
2008-09-03 23:09:04 +0000

    Fix ptrace.cpu_tools for Windows: use platform.architecture() instead of uname()

--- ptrace/cpu_info.py
+++ ptrace/cpu_info.py
@@ -1,4 +1,9 @@
-from os import uname
+try:
+    from os import uname
+    HAS_UNAME = True 
+except ImportError:
+    HAS_UNAME = False 
+    from platform import architecture
 from sys import byteorder
 from ctypes import sizeof, c_void_p
 
@@ -12,16 +17,28 @@ else:
     CPU_WORD_SIZE = 4 # bytes
     CPU_MAX_UINT = 0xffffffff
 
-# machine type from uname -m:
-# "ppc" -> PowerPC 32 bits
-# "i686" -> Intel 32 bits
-# "x86_64" -> Intel 64 bits
-_machine = uname()[4]
-CPU_PPC32 = (_machine == 'ppc')
-CPU_PPC64 = (_machine == 'ppc64')
-CPU_I386 = (_machine in ("i386", "i686"))    # compatible Intel 32 bits
-CPU_X86_64 = (_machine == "x86_64")  # compatible Intel 64 bits
-del _machine
+if HAS_UNAME:
+    # guess machine type using uname()
+    _machine = uname()[4]
+    CPU_PPC32 = (_machine == 'ppc')
+    CPU_PPC64 = (_machine == 'ppc64')
+    CPU_I386 = (_machine in ("i386", "i686"))    # compatible Intel 32 bits
+    CPU_X86_64 = (_machine == "x86_64")  # compatible Intel 64 bits
+    del _machine
+else:
+    # uname() fallback for Windows
+    # I hope that your Windows doesn't run on PPC32/PPC64
+    CPU_PPC32 = False 
+    CPU_PPC64 = False 
+    CPU_I386 = False 
+    CPU_X86_64 = False 
+    bits, linkage = architecture()
+    if bits == '32bit':
+        CPU_I386 = True
+    elif bits == '64bit':
+        CPU_X86_64 = True
+    else:
+        raise ValueError("Unknown architecture bits: %r" % bits) 
 
 CPU_INTEL = (CPU_I386 or CPU_X86_64)
 CPU_POWERPC = (CPU_PPC32 or CPU_PPC64)
