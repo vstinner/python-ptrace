cab7127e0b6d3e68c528beaf6b6cd31dfd11cca4
haypo <devnull@localhost>
2008-09-04 23:08:14 +0000

    Commit ptrace.mockup, needed by unit tests

---
+++ ptrace/mockup.py
@@ -0,0 +1,10 @@
+class FakeProcess:
+    def __init__(self):
+        self.regs = {}
+
+    def setreg(self, name, value):
+        self.regs[name] = value
+
+    def getreg(self, name):
+        return self.regs[name]
+
