b760a14150ca9eacd9a82497720f7a691b7209c0
Victor Stinner <vstinner@python.org>
2021-03-17 21:22:57 +0100

    post-release: set version to 0.9.9

--- ptrace/version.py
+++ ptrace/version.py
@@ -1,5 +1,5 @@
 PACKAGE = "python-ptrace"
-VERSION = (0, 9, 8)
+VERSION = (0, 9, 9)
 __version__ = '.'.join(map(str, VERSION))
 WEBSITE = "http://python-ptrace.readthedocs.io/"
 LICENSE = "GNU GPL v2"
