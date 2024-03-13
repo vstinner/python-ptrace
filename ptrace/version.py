fe53fff3c08fd669cc6e4fc3507117e62df6f11a
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2024-03-13 18:02:04 +0300

    Post-release

--- ptrace/version.py
+++ ptrace/version.py
@@ -1,5 +1,5 @@
 PACKAGE = "python-ptrace"
-VERSION = (0, 9, 9)
+VERSION = (0, 9, 10)
 __version__ = '.'.join(map(str, VERSION))
 WEBSITE = "http://python-ptrace.readthedocs.io/"
 LICENSE = "GNU GPL v2"
